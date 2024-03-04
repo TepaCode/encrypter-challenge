e06ebd83d08c0efbcb3a011eef2876abb41abbb9
Tepayol <tepa.code@gmail.com>
2024-03-03 22:46:39 -0600

    03032024 Incorporacion de codigo JS

--- Js-encrypter.js
+++ Js-encrypter.js
@@ -1,2 +1,24 @@
 
 //alert("Hola mundo");
+
+//Declaración de variables
+let letterA = "ai";
+let letterE = "enter";
+let letterI = "imes";
+let letterO = "ober";
+let letterU = "ufat";
+
+
+
+function showMsg(){
+    let msgIngresado = document.getElementById("textInput").value;
+    let msgOutput = document.getElementById("textOutput");
+
+   
+    msgOutput.value=msgIngresado;
+
+}
+
+function reloadSite(){
+    document.location.reload()
+}
