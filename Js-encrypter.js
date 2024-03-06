33eab544ec369e12f43823fecb82e696b80c4547
Tepayol <tepa.code@gmail.com>
2024-03-05 20:33:37 -0600

    05032024 - Incorporación de codigo JS - Mucho que hacer aún!

--- Js-encrypter.js
+++ Js-encrypter.js
@@ -2,6 +2,7 @@
 //alert("Hola mundo");
 
 //Declaración de variables
+
 let letterA = "ai";
 let letterE = "enter";
 let letterI = "imes";
@@ -10,15 +11,70 @@ let letterU = "ufat";
 
 
 
-function showMsg(){
-    let msgIngresado = document.getElementById("textInput").value;
-    let msgOutput = document.getElementById("textOutput");
 
-   
+//Declaración de formulas
+
+function captureMsg(){
+    let msgIngresado = document.getElementById("textInput").value;
+    msgIngresado.toLowerCase();
+
+    sendTextEncrypter(msgIngresado);
+    //let frameText = document.querySelector("div");
+    //frameText.style.backgroundColor = gray;
+
+}
+
+function sendTextEncrypter(msgIngresado){
+
+    let msgOutput = document.getElementById("textOutput");
     msgOutput.value=msgIngresado;
+
+
+    let setStyle = document.getElementsById("containerOutput");
+    setStyle.style.backgroundImage = none;
+
+    setStyle.style.backgroundColor = gray;
+
+    msgIngresado.value= "55555";
+
+}
+
+function encrypter(){
+    msgIngresado = msgIngresado.tolowerCase();
+
+    for(let x in msgIngresado){
+        if(msgIngresado.includes("a")){
+            msgIngresado.replaceAll(letterA);
+        }
+        if(msgIngresado.includes("e")){
+            msgIngresado.replaceAll(letterE);
+        }
+        if(msgIngresado.includes("i")){
+            msgIngresado.replaceAll(letterI);
+        }
+        if(msgIngresado.includes("o")){
+            msgIngresado.replaceAll(letterO);
+        }
+        if(msgIngresado.includes("u")){
+            msgIngresado.replaceAll(letterU);
+        }
+    }
+}
+
+
+function testButton(){
+    msgIngresado.value = "";
+
+}
+    
+
+function copyText(){
+    let portapapeles = document.getElementById("textOutput").value;
+    alert(`Mensaje copiado: ${portapapeles}`);
 
 }
 
 function reloadSite(){
     document.location.reload()
 }
+
